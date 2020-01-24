f3248cc99fe26c9e13e49ed794253c7b6a571dc3
DarkBrotger4756 <39713782+DarkBrotger4756@users.noreply.github.com>
2020-01-24 20:43:55 +0600

    Create Easy-Spymer-Group.sh

---
+++ Easy-Spymer-Group.sh
@@ -0,0 +1,161 @@
+clear
+
+choice(){
+echo "Выбери жертву:"
+echo "[1] Тимур Калиев"
+echo "[2] Даурен Муздыбаев"
+echo "[3] Диана"
+echo "[4] Борис Диппнер"
+echo "[5] Ева Морозова" 
+echo "[6] Кирилл Поляков"
+echo "[7] Мейрам"
+echo "[8] Семсер Шалкар"
+echo "[9] Вадим Горбунов"
+echo "[10] Роман"
+echo "[11] Мади Тулегенов"
+echo "[12] Дамир Калиев"
+echo "[13] Аян"
+echo "[4756] Все"
+echo "[0] выход"
+printf "Твой выбор - "
+read pray
+case $pray in
+1)
+echo"Тимур" 
+spymer 87089684953
+clear
+echo "DDOS Тимур"
+;;
+2)
+echo"Даурен" 
+spymer 87475008775
+clear
+echo "DDOS Даурен"
+;;
+3)
+echo"Диана"
+ spymer 87475665927
+clear
+echo "DDOS Диана"
+;;
+4)
+echo"Диппер" 
+spymer 87017574443
+clear
+echo "DDOS Борис"
+;;
+5)
+echo"Ева"
+ spymer 87057546345
+clear
+echo "DDOS Ева"
+;;
+6)
+echo"Кириллллл" 
+spymer 87087097474
+clear
+echo "DDOS Кирилл"
+;;
+7)
+echo"Мейрам" 
+spymer 87783797288
+clear
+echo "DDOS Мейрам"
+;;
+8)
+echo"Семсер" 
+spymer 87009764413
+clear
+echo "DDOS Семсер"
+;;
+9)
+echo"Вадим" 
+spymer 87073023695
+clear
+echo "DDOS Вадим"
+;;
+10)
+echo"Роман" 
+spymer 87088010304
+clear
+echo "DDOS Роман"
+;;
+11)
+echo"Мади" spymer 87713721355
+clear
+echo "DDOS Мади"
+;;
+12)
+echo"Дамир К" 
+spymer 87753457953
+clear
+echo "DDOS Дамир Калиев"
+;;
+13)
+echo"Аян" 
+spymer 87755254342
+clear
+echo FULL_GROUP_IS_SMS-DDOS-ING
+;;
+0)
+exit
+;;
+4756)
+echo"Тимур" 
+spymer 87089684953
+clear
+echo "DDOS_SIB-19-4"
+echo"Даурен" 
+spymer 87475008775
+clear
+echo "DDOS_SIB-19-4"
+echo"Диана" 
+spymer 87475665927
+clear
+echo "DDOS_SIB-19-4"
+echo"Диппер" 
+spymer 87017574443
+clear
+echo "DDOS_SIB-19-4"
+echo"Ева" 
+spymer 87057546345
+clear
+echo "DDOS_SIB-19-4"
+echo"Кириллллл" 
+spymer 87087097474
+clear
+echo "DDOS_SIB-19-4"
+echo"Мейрам" 
+spymer 87783797288
+clear
+echo "DDOS_SIB-19-4"
+echo"Семсер" 
+spymer 87009764413
+clear
+echo "DDOS_SIB-19-4"
+echo"Вадим" 
+spymer 87073023695
+clear
+echo "DDOS_SIB-19-4"
+echo"Роман" 
+spymer 87088010304
+clear
+echo "DDOS_SIB-19-4"
+echo"Мади" 
+spymer 87713721355
+clear
+echo "DDOS_SIB-19-4"
+echo"Дамир К" 
+spymer 87753457953
+clear
+echo "DDOS_SIB-19-4"
+echo"Аян"
+ spymer 87755254342
+clear
+echo FULL_GROUP_IS_SMS-DDOS-ING
+;;
+esac
+clear
+choice
+}
+choice
